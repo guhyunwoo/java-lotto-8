e72f26444473a6db46f85795754416c52ca8d992
guhyunwoo <realization0327@gmail.com>
2025-11-03 20:42:39 +0900

    feat: 로또 구입 예외 추가
    - NumberOfTicketsExtractor에 음수 검증 추가
    - LOTTO_PRICE_IS_LESS_THAN_ZERO 에러 메시지 추가

--- src/main/java/lotto/domain/NumberOfTicketsExtractor.java
+++ src/main/java/lotto/domain/NumberOfTicketsExtractor.java
@@ -6,13 +6,24 @@ public class NumberOfTicketsExtractor {
     private static final int TICKET_UNIT_PRICE = 1000;
 
     public static int extract(int price) {
-        ensurePriceIsDivisible(price);
+        ensurePrice(price);
         return price / TICKET_UNIT_PRICE;
+    }
+
+    private static void ensurePrice(int price) {
+        ensurePriceIsDivisible(price);
+        ensurePriceIsBiggerThanZero(price);
     }
 
     private static void ensurePriceIsDivisible(int price) {
         if(price % TICKET_UNIT_PRICE != 0) {
             throw new IllegalArgumentException(ErrorMessage.LOTTO_PRICE_IS_NOT_DIVISIBLE.content);
+        }
+    }
+
+    private static void ensurePriceIsBiggerThanZero(int price) {
+        if(price < 0) {
+            throw new IllegalArgumentException(ErrorMessage.LOTTO_PRICE_IS_LESS_THAN_ZERO.content);
         }
     }
 }
